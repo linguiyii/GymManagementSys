d899a023309a6a469b83ca89dc7b83a6b87fbd64
X2006620 <sysmessage@mail.foxconn.com>
2024-06-28 16:28:00 +0800

    添加主页查询用户、滑动item删除用户功能。这两个功能暂时放在主页，以后应该会移动到个人中心

--- app/src/main/java/edu/demo/gymsys/activity/HomeActivity.java
+++ app/src/main/java/edu/demo/gymsys/activity/HomeActivity.java
@@ -1,42 +1,80 @@
 package edu.demo.gymsys.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import edu.demo.gymsys.R;
+import edu.demo.gymsys.activity.login.LoginActivity;
 import edu.demo.gymsys.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        // 查找 Toolbar 并设置为 ActionBar
+
+        setupToolbar();
+        setupNavigation();
+        setupBackPressedHandler();
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+    }
+
+    private void setupNavigation() {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    private void setupBackPressedHandler() {
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (doubleBackToExitPressedOnce) {
+                    exitToLogin();
+                    return;
+                }
+
+                doubleBackToExitPressedOnce = true;
+                Toast.makeText(HomeActivity.this, "再按一次退出登录", Toast.LENGTH_SHORT).show();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doubleBackToExitPressedOnce = false;
+                    }
+                }, 2000); // 设置间隔时间，单位为毫秒
+            }
+        });
+    }
+
+    private void exitToLogin() {
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
